949f345102b4b48ae1fa5c627b0d76eafc7943b0
mdrizky <mdrizky240708@gmail.com>
2026-05-09 09:46:19 +0700

    fix: add 'use client' to SocialLinks to fix build error

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect } from "react";
 import { supabase } from "@/lib/supabase";
 import { FaWhatsapp, FaInstagram, FaGithub, FaLinkedin, FaTiktok, FaYoutube } from "react-icons/fa";
